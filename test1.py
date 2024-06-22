bf068f3164fdd3ab65b570b91545f32973b61276
vk2012022 <99138478+vk2012022@users.noreply.github.com>
2024-06-22 15:06:47 +0300

    add new code 3

---
+++ test1.py
@@ -0,0 +1,11 @@
+from selenium import webdriver  # pip install selenium
+from selenium.webdriver import Keys
+#Библиотека, которая позволяет вводить данные на сайт с клавиатуры
+from selenium.webdriver.common.by import By
+#Библиотека с поиском элементов на сайте
+import time
+
+browser = webdriver.Chrome()  # Optional argument, if not specified will search path.
+browser.get('https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0')
+assert "Википедия" in browser.title
+time.sleep(10)
